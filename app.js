794bd56eb04d7d77e5ae514faaa63bec4a8b675e
Lucas Chiappinigit <chiappinilucas@gmail.com>
2023-06-14 19:45:58 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,68 @@
+// JSON.parce
+
+// const jsonStr = '{"name": "John", "age": 30, "city": "New York"}';
+// const obj = JSON.parse(jsonStr);
+// console.log(obj.name); // Output: John
+// console.log(obj.age); // Output: 30
+// console.log(obj.city); // Output: New York
+
+// JSON.stringify
+
+// const obj = { name: "John", age: 30, city: "New York" };
+// const jsonStr = JSON.stringify(obj);
+// console.log(jsonStr); // Output: {"name":"John","age":30,"city":"New York"}
+
+//Manejo de JSON, agregar nuevos valores y modificar
+
+// const jsonStr = '{"name": "John", "age": 30, "city": "New York"}';
+// const obj = JSON.parse(jsonStr);
+
+// obj.age = 31; // Modificar la propiedad "age"
+// obj.country = "USA"; // Agregar una nueva propiedad
+
+// const updatedJsonStr = JSON.stringify(obj);
+// console.log(updatedJsonStr);
+
+
+//Find ejemplo:
+
+// const numbers = [1, 2, 3, 4, 5];
+// const evenNumber = numbers.find((number) => number % 2 === 0);
+
+// console.log(evenNumber); // Output: 2
+
+//Filter/ PEÑAROL
+
+// const numbers = [1, 2, 3, 4, 5];
+// const evenNumbers = numbers.filter((number) => number % 2 === 0);
+
+// console.log(evenNumbers); // Output: [2, 4]
+
+//Map 
+
+// const numbers = [1, 2, 3, 4, 5];
+// const squaredNumbers = numbers.map((number) => number * number);
+
+// console.log(squaredNumbers); // Output: [1, 4, 9, 16, 25]
+
+//Reduce
+
+// const numbers = [1, 2, 3, 4, 5];
+// const sum = numbers.reduce((accumulator, current) => accumulator + current, 0);
+
+// console.log(sum); // Output: 15
+
+
+const colores = ['rojo', 'verde', 'azul', 'amarillo', 'naranja', 'rosa', 'violeta', 'rojo', 'verde', 'azul',];
+
+// un ejemplo de un array con muchos colores y quiero filtrarlo dependiendo de lo que el usuario escriba en un input con javaScript
+const Filter = () => {
+    let colorValue = document.querySelector("#iColor").value;
+    const colorFilter = colores.find((color) => color == colorValue);
+    if(colorFilter!= undefined){
+        document.getElementById("resultado").innerHTML = `<ul id="resultado"><li>TENEMOS EL COLOR:${colorFilter}</li></ul>`;
+    }
+    else{
+        document.getElementById("resultado").innerHTML = `<ul id="resultado"><li>NO TENEMOS ESE COLOR</li></ul>`; 
+    }
+}
